9629241d38112eb11ba2e7cce83e6b070a25dc6c
Kevin Conway <kconway@atlassian.com>
2019-05-10 10:44:18 -0500

    Update to latest serverfull usage

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"os"
 
 	"github.com/asecurityteam/awsconfig-filterd/pkg/filter"
-	"github.com/asecurityteam/awsconfig-filterd/pkg/handlers/v1"
+	v1 "github.com/asecurityteam/awsconfig-filterd/pkg/handlers/v1"
 	"github.com/asecurityteam/runhttp"
-	serverfull "github.com/asecurityteam/serverfull/pkg"
-	serverfulldomain "github.com/asecurityteam/serverfull/pkg/domain"
+	"github.com/asecurityteam/serverfull"
 	"github.com/asecurityteam/settings"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
@@ -30,15 +28,12 @@ func main() {
 		StatFn:         runhttp.StatFromContext,
 		ConfigFilterer: resourceTypeFilterer,
 	}
-	handlers := map[string]serverfulldomain.Handler{
-		"filter": lambda.NewHandler(configFilterHandler.Handle),
+	handlers := map[string]serverfull.Function{
+		"filter": serverfull.NewFunction(configFilterHandler.Handle),
 	}
 
-	rt, err := serverfull.NewStatic(ctx, source, handlers)
-	if err != nil {
-		panic(err.Error())
-	}
-	if err := rt.Run(); err != nil {
+	fetcher := &serverfull.StaticFetcher{Functions: handlers}
+	if err := serverfull.Start(ctx, source, fetcher); err != nil {
 		panic(err.Error())
 	}
 }
