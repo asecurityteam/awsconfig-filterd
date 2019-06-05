742b760a90a594a2c6d81caebd08b2fa77d94648
Kevin Conway <kconway@atlassian.com>
2019-06-05 15:10:56 -0500

    Use components in main
    The handler setup is now a component that aggregates all possible
    plugins for the app.
    
    Producer and HTTP client plugins are introduced.
    
    Add an env var switch for activating Lambda mode.

--- main.go
+++ main.go
@@ -6,10 +6,70 @@ import (
 
 	"github.com/asecurityteam/awsconfig-filterd/pkg/filter"
 	v1 "github.com/asecurityteam/awsconfig-filterd/pkg/handlers/v1"
+	"github.com/asecurityteam/components"
 	"github.com/asecurityteam/runhttp"
 	"github.com/asecurityteam/serverfull"
 	"github.com/asecurityteam/settings"
 )
+
+type Config struct {
+	Filter     *filter.FilterConfig
+	Producer   *components.ProducerConfig
+	LambdaMode bool `description:"Use the Lambda SDK to start the system."`
+}
+
+func (*Config) Name() string {
+	return "awsconfigfilterd"
+}
+
+type Component struct {
+	Filter   *filter.FilterComponent
+	Producer *components.ProducerComponent
+}
+
+func NewComponent() *Component {
+	return &Component{
+		Filter:   filter.NewFilterComponent(),
+		Producer: components.NewProducerComponent(),
+	}
+}
+
+func (c *Component) Settings() *Config {
+	return &Config{
+		Filter:   c.Filter.Settings(),
+		Producer: c.Producer.Settings(),
+	}
+}
+
+func (c *Component) New(ctx context.Context, conf *Config) (func(context.Context, settings.Source) error, error) {
+	f, err := c.Filter.New(ctx, conf.Filter)
+	if err != nil {
+		return nil, err
+	}
+	p, err := c.Producer.New(ctx, conf.Producer)
+	if err != nil {
+		return nil, err
+	}
+
+	filterHandler := &v1.ConfigFilter{
+		LogFn:          runhttp.LoggerFromContext,
+		StatFn:         runhttp.StatFromContext,
+		ConfigFilterer: f,
+		Producer:       p,
+	}
+	handlers := map[string]serverfull.Function{
+		"filter": serverfull.NewFunction(filterHandler.Handle),
+	}
+	fetcher := &serverfull.StaticFetcher{Functions: handlers}
+	if conf.LambdaMode {
+		return func(ctx context.Context, source settings.Source) error {
+			return serverfull.StartLambda(ctx, source, fetcher, "filter")
+		}, nil
+	}
+	return func(ctx context.Context, source settings.Source) error {
+		return serverfull.StartHTTP(ctx, source, fetcher)
+	}, nil
+}
 
 func main() {
 	source, err := settings.NewEnvSource(os.Environ())
@@ -17,23 +77,13 @@ func main() {
 		panic(err.Error())
 	}
 	ctx := context.Background()
-	resourceFiltererComponent := &filter.ResourceTypeFiltererComponent{}
-	resourceTypeFilterer := new(filter.ResourceTypeFilterer)
-	err = settings.NewComponent(ctx, source, resourceFiltererComponent, resourceTypeFilterer)
+	runner := new(func(context.Context, settings.Source) error)
+	cmp := NewComponent()
+	err = settings.NewComponent(ctx, source, cmp, runner)
 	if err != nil {
 		panic(err.Error())
 	}
-	configFilterHandler := v1.ConfigFilterHandler{
-		LogFn:          runhttp.LoggerFromContext,
-		StatFn:         runhttp.StatFromContext,
-		ConfigFilterer: resourceTypeFilterer,
-	}
-	handlers := map[string]serverfull.Function{
-		"filter": serverfull.NewFunction(configFilterHandler.Handle),
-	}
-
-	fetcher := &serverfull.StaticFetcher{Functions: handlers}
-	if err := serverfull.Start(ctx, source, fetcher); err != nil {
+	if err := (*runner)(ctx, source); err != nil {
 		panic(err.Error())
 	}
 }
